c2c9c6f4a4e6bb732f2554c623a7c577a3c52480
jonasmalacofilho <jonasmalacofilho@gmail.com>
2013-04-28 19:31:56 -0300

    Improved TestFloat.

--- basetest/TestFloat.hx
+++ basetest/TestFloat.hx
@@ -3,37 +3,68 @@ class TestFloat extends gtf.Test {
 	public function new() {}
 
 	@test function testInfGtLt() {
-		assertTrue( Math.NEGATIVE_INFINITY < Math.POSITIVE_INFINITY );
-		assertTrue( Math.POSITIVE_INFINITY > Math.NEGATIVE_INFINITY );
+		// basic ordering
 		assertTrue( 0. < Math.POSITIVE_INFINITY );
 		assertTrue( 0. > Math.NEGATIVE_INFINITY );
+		assertTrue( Math.NEGATIVE_INFINITY < Math.POSITIVE_INFINITY );
+		assertTrue( Math.POSITIVE_INFINITY > Math.NEGATIVE_INFINITY );
+		// +/-inf is NOT great/less than itself
+		assertFalse( Math.POSITIVE_INFINITY > Math.POSITIVE_INFINITY );
+		assertFalse( Math.NEGATIVE_INFINITY < Math.NEGATIVE_INFINITY );
 	}
 
-	@test function behaveInfEqNeq() {
-		assertTrue( Math.POSITIVE_INFINITY == Math.POSITIVE_INFINITY );
-		assertTrue( Math.NEGATIVE_INFINITY == Math.NEGATIVE_INFINITY );
-		assertFalse( Math.POSITIVE_INFINITY != Math.POSITIVE_INFINITY );
-		assertFalse( Math.NEGATIVE_INFINITY != Math.NEGATIVE_INFINITY );
+	@test function testInfEqNeq() {
+		// inf == inf
+		assertEquals( Math.POSITIVE_INFINITY, Math.POSITIVE_INFINITY );
+		assertEquals( Math.NEGATIVE_INFINITY, Math.NEGATIVE_INFINITY );
 	}
 
 	@test function testInfNaN() {
+		// NaN != inf
 		assertFalse( Math.isNaN( Math.POSITIVE_INFINITY ) );
 		assertFalse( Math.isNaN( Math.NEGATIVE_INFINITY ) );
 		assertFalse( Math.isFinite( Math.NaN ) );
+		// +/-inf is NOT great/less than NaN
+		assertFalse( Math.POSITIVE_INFINITY > Math.NaN );
+		assertFalse( Math.POSITIVE_INFINITY < Math.NaN );
+		assertFalse( Math.NEGATIVE_INFINITY > Math.NaN );
+		assertFalse( Math.NEGATIVE_INFINITY < Math.NaN );
 	}
 
 	@test function testNaNEqNeq() {
+		// NaN is NaN
 		assertTrue( Math.isNaN( Math.NaN ) );
-		assertFalse( Math.NaN == Math.NaN );
-		assertTrue( Math.NaN != Math.NaN );
+		// NaN != NaN && NaN !> NaN && NaN !< NaN
+		assertDifferent( Math.NaN, Math.NaN );
+		assertFalse( Math.NaN < Math.NaN );
+		assertFalse( Math.NaN > Math.NaN );
 	}
 
 	@test function testExceptions() {
+		// sqrt( -1 ) is NaN
 		assertTrue( Math.isNaN( Math.sqrt( -1. ) ) );
+		// 1./0. is signed inf
 		assertTrue( !Math.isFinite( 1./0. ) );
 		assertEquals( Math.POSITIVE_INFINITY, 1./0. );
 		assertTrue( !Math.isFinite( (-1.)/0. ) );
 		assertEquals( Math.NEGATIVE_INFINITY, (-1.)/0. );
+	}
+
+	@test function testInfSumSubs() {
+		// +inf +- any number is +inf
+		assertEquals( Math.POSITIVE_INFINITY, Math.POSITIVE_INFINITY + 1. );
+		assertEquals( Math.POSITIVE_INFINITY, Math.POSITIVE_INFINITY - 1. );
+		assertEquals( Math.POSITIVE_INFINITY, Math.POSITIVE_INFINITY + Math.POSITIVE_INFINITY );
+		// -inf +- any number is -inf
+		assertEquals( Math.NEGATIVE_INFINITY, Math.NEGATIVE_INFINITY + 1. );
+		assertEquals( Math.NEGATIVE_INFINITY, Math.NEGATIVE_INFINITY - 1. );
+		assertEquals( Math.NEGATIVE_INFINITY, Math.NEGATIVE_INFINITY + Math.NEGATIVE_INFINITY );
+		// +inf - (-inf) is still +inf (inf+inf)
+		assertEquals( Math.POSITIVE_INFINITY, Math.POSITIVE_INFINITY - Math.NEGATIVE_INFINITY );
+		// -inf - (+inf) is still -inf (-inf-inf)
+		assertEquals( Math.NEGATIVE_INFINITY, Math.NEGATIVE_INFINITY - Math.POSITIVE_INFINITY );
+		// +inf + (-inf) is NaN
+		assertTrue( Math.isNaN( Math.POSITIVE_INFINITY + Math.NEGATIVE_INFINITY ) );
 	}
 
 }
